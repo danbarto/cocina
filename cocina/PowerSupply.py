47427ce684f5138520adb1f56d6396de2a976cf4
dspitzba <daniel.spitzbart@cern.ch>
2023-03-03 13:28:18 -0500

    cosmetics and adding CI test stand PS

--- cocina/PowerSupply.py
+++ cocina/PowerSupply.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import siglent_psu_api.siglent_psu_api as siglent
-from cocina.colors import green, red, yellow, dummy
+from .colors import green, red, yellow, dummy
 
-hline = "| " + "-"*20 + " |"
+topline = "┏━" + "━"*20 + "━┓"
+botline = "┗━" + "━"*20 + "━┛"
 
 class PowerSupply:
     def __init__(self, name, ip):
@@ -26,14 +27,13 @@ class PowerSupply:
 
 
         for channel in self.channels:
-            print ()
             if status[channel].value == 1: colored = green
             elif status[channel].value == 0: colored = red
-            print(colored(hline))
-            print(colored( "| {:10}{:10} |".format("Channel", channel) ))
-            print(colored( "| {:10}{:10} |".format("Voltage", res[channel]["Voltage"]) ))
-            print(colored( "| {:10}{:10} |".format("Current", res[channel]["Current"]) ))
-            print(colored(hline))
+            print(colored(topline))
+            print(colored( "┃ {:10}{:10} ┃".format("Channel", channel) ))
+            print(colored( "┃ {:10}{:10} ┃".format("Voltage", res[channel]["Voltage"]) ))
+            print(colored( "┃ {:10}{:10} ┃".format("Current", res[channel]["Current"]) ))
+            print(colored(botline))
         #r = self.s.measure(ch = siglent.CHANNEL.CH1, parameter = siglent.PARAMETER.VOLTAGE)
         #print(r)
         #
--- lab.py
+++ lab.py
@@ -7,6 +7,11 @@ if __name__ == '__main__':
 
     ps1 = PowerSupply("Readout", "192.168.2.1")
     ps2 = PowerSupply("Emulator", "192.168.2.2")
+    ps3 = PowerSupply("CI", "192.168.2.3")
 
+    print ("\nPS 1 (RBs)")
     ps1.monitor()
+    print ("\nPS 2 (Emulators)")
     ps2.monitor()
+    print ("\nPS 3 (CI)")
+    ps3.monitor()
