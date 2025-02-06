ea10d881724c459bd116450743264582e35971bf
danbarto <daniel.spitzbart@hey.com>
2025-02-06 09:00:54 +0100

    reverting back to classic socket

--- cocina/SkippyDevice.py
+++ cocina/SkippyDevice.py
@@ -5,29 +5,33 @@ Parent class for all SCPI devices
 
 import logging
 import threading
-import zmq
+import time
 import socket
 
 class SkippyDevice():
-    def __init__(self, ip: str, port: int, name: str = ""):
+    def __init__(self, ip: str, port: int, name: str = "", timeout: int = 1, wait: int = 0):
         '''
-        Initialize a SCPI device
+        Initialize a SCPI device, with a default timeout for socket transactions.
+        For some (slow?) devices a wait time between send and receive is necessary.
 
         Parameters:
             ip (str): IP Address of the device
             port (int): port to use for SCPI connection
             name (str): arbitrary name used for the python instance of the device
+            timeout (int): timeout of socket transaction in seconds
+            wait (int): wait time between send and receive
         '''
 
         self.name       = name
         self.ip         = ip
         self.port       = port
         self.dev        = None
+        self.timeout    = timeout
+        self.wait       = wait
 
         self.logger     = logging.getLogger(__name__)
         self.lock       = threading.Lock()
 
-        print("Connect")
         self.connect()
 
     def connect(self) -> bool:
@@ -38,30 +42,41 @@ class SkippyDevice():
             bool: True for a successful connection
         '''
         with self.lock:
-            context = zmq.Context()
-            self.dev = context.socket(zmq.REQ)
-            self.dev.connect(f"tcp://{self.ip}:{self.port}")
+            self.dev = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.dev.settimeout(self.timeout)
+            self.dev.connect((self.ip, self.port))
+            #context = zmq.Context()
+            #self.dev = context.socket(zmq.REQ)
+            #self.dev.connect(f"tcp://{self.ip}:{self.port}")
             self.logger.info(f"Connected to SCPI Device {self.name} @ {self.ip}:{self.port}")
         if self.dev:
             return True
         else:
             return False
 
-    def send(self, msg: str) -> str:
+    def send(self, msg: str, read: bool=True) -> str:
         '''
         Send a message to the device
 
         Parameters:
             msg (str): The message to be sent to the device
+            read (bool): Read back reply
+            wait (int): wait time between send and receive
+
 
         Returns:
             str: Response from the device
         '''
         if not self.dev:
             self.connect()
-        self.dev.send_string(msg, zmq.NOBLOCK)
-        res = self.dev.recv().decode("utf-8")
-        return res
+        self.dev.sendall(msg.encode('utf-8'))
+        if self.wait>0:
+            time.sleep(self.wait)
+        if read:
+            res = self.dev.recv(4096).decode("utf-8").strip()
+            return res
+        else:
+            return ""
 
     def close(self):
         '''
